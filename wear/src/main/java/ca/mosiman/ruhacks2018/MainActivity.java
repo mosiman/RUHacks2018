4637f533f429099e3d4963c233834f43ba533193
mosiman <dillonchan97@gmail.com>
2018-05-26 09:23:57 -0400

    stuff is working rn

--- wear/src/main/java/ca/mosiman/ruhacks2018/MainActivity.java
+++ wear/src/main/java/ca/mosiman/ruhacks2018/MainActivity.java
@@ -1,21 +1,36 @@
 package ca.mosiman.ruhacks2018;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements SensorEventListener{
 
-    private TextView mTextView;
+    private TextView tv_heartRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        tv_heartRate = (TextView) findViewById(R.id.dispHeartRate);
+        String message = "hearto rato:";
+        tv_heartRate.setText(message);
 
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }

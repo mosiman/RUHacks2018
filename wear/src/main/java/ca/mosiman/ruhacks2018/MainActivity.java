38763c736548cf15c0923cb162f276d7e7cc40bf
mosiman <dillonchan97@gmail.com>
2018-05-26 10:31:11 -0400

    heart rate sensor working. note to self, have to set permissions on wear!

--- wear/src/main/java/ca/mosiman/ruhacks2018/MainActivity.java
+++ wear/src/main/java/ca/mosiman/ruhacks2018/MainActivity.java
@@ -1,15 +1,19 @@
 package ca.mosiman.ruhacks2018;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends WearableActivity implements SensorEventListener{
 
     private TextView tv_heartRate;
+    SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +21,34 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         setContentView(R.layout.activity_main);
 
         tv_heartRate = (TextView) findViewById(R.id.dispHeartRate);
-        String message = "hearto rato:";
+        String message = "heartu ratuh:";
         tv_heartRate.setText(message);
 
         // Enables Always-on
         setAmbientEnabled();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
     }
 
+    protected void onResume() {
+        super.onResume();
+        Sensor heartSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        sensorManager.registerListener(this,heartSensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        //h.removeCallbacks(runnable); //stop handler when activity not visible
+    }
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            String msg = "" + (int)event.values[0];
+            tv_heartRate.setText(msg);
+        }
     }
 
     @Override
